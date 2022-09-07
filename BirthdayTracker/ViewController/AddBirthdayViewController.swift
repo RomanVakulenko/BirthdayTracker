467ea9f724a83739e00be15244b01b19748ecd7b
RomanVakulenko <vakula_48@mail.ru>
2022-09-07 17:02:41 +0300

    closureTranferedDataFrom2ndVCTo1st

--- BirthdayTracker/ViewController/AddBirthdayViewController.swift
+++ BirthdayTracker/ViewController/AddBirthdayViewController.swift
@@ -12,7 +12,7 @@ class AddBirthdayViewController: UIViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var birthdayPicker: UIDatePicker!
     
-    var completion: ((Birthday) -> Void)? // 1создали свойство (тип замыкание), указываем, что свойство принимает в себя аргумент Birthday. ?тк оно сразу не существует
+    var completion: ((Birthday) -> Void)? // 1создали свойство (его тип функция - замыкание), указываем, что функция принимает в себя аргумент Birthday. ?тк замыкание еще не существует, не инициализировано, будет инициализировано когда мы в него передадим newBirtday -экземпляр класса Birthday
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,19 +27,17 @@ class AddBirthdayViewController: UIViewController {
        
         let newBirtday = Birthday(firstName: firstNameAB, lastName: lastNameAB, birthdate: birthDateAB) //в новый ДР записываем введенное
        
-        completion?(newBirtday) // 2передаем аргумент в фунцию-вКлоужер
+        completion?(newBirtday) // 2вызываем функцию, передавая в неё аргумент (частный вид делегата)
         navigationController?.popViewController(animated: true) //по нажатию на save 2ой VC достается из стека и показывается
         
         print("Создан ДР, Имя: \(newBirtday.firstName), Фамилия: \(newBirtday.lastName)")
         print("ДР: \(newBirtday.birthdate)")
     }
-    
-    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
-
+}
         
         
-// это для варианта с PresentModaly из книги:
+// dismiss нужен для варианта с PresentModaly из книги:
 //        dismiss(animated: true, completion: nil) //отключает изображающийся в настоящее время контроллер представлений, принимает 2 параметра: Первый — animated для анимации закрывающегося экрана. При передаче значения true экран AddBirthday соскальзывает и исчезает. Второй параметр — это опционал замыкания с названием completion - это блок кода, передаваемый функции, используется в том случае, если у вас есть код, который должен запуститься после отключения контроллера представлений. Поскольку пока делать ничего не нужно - присвоили nil.
 //
-    }
-}
+//    }
+
--- BirthdayTracker/ViewController/BirthdaysTableViewController.swift
+++ BirthdayTracker/ViewController/BirthdaysTableViewController.swift
@@ -29,8 +29,8 @@ class BirthdaysTableViewController: UITableViewController {
     private func nextVC () {
         let stb = UIStoryboard(name: "Main", bundle: .main) // создаем Storyboard и VC
         let vc = stb.instantiateViewController(withIdentifier: "AddBirthdayViewController")
-//задаем в замыкание функцию (указываем что захватить и перезагрузить tableView)
-        (vc as? AddBirthdayViewController)?.completion = { [weak self] birthday in
+//показываем контроллер; обращаемся к комплишн и задаем в замыкание функцию (указываем что захватить и перезагрузить tableView)
+        (vc as? AddBirthdayViewController)?.completion = { [weak self] birthday in //аргумент birthday добавляем в массив и перезагружаем тейблВью
             self?.birthdays.append(birthday)
             self?.tableView.reloadData()
         }
@@ -57,51 +57,4 @@ class BirthdaysTableViewController: UITableViewController {
 
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
